dd9b24bebbe66d0a0937056fd17eb20699790988
Dan Kuester <dkuester@nist.gov>
2017-02-21 09:49:35 -0700

    more modernizing the power sensor

--- remotelets_drivers/power_sensors.py
+++ remotelets_drivers/power_sensors.py
@@ -19,26 +19,25 @@ class KeysightU2040XSeries(rlts.VISAInstrument):
             \\jake\bla\ajlsdjfalsdjf\Calibrations\cooldata.dat
     '''
     class state(rlts.VISAInstrument.state):
-        initiate_continuous     = rlts.SCPI(rlts.Bool(), 'INIT:CONT')    
-        output_trigger          = rlts.SCPI(rlts.Bool(),                                         'OUTP:TRIG')
+        initiate_continuous     = rlts.SCPI(rlts.Bool(), 'INIT:CONT')
+        output_trigger          = rlts.SCPI(rlts.Bool(), 'OUTP:TRIG')
         trigger_source          = rlts.SCPI(rlts.EnumBytes(['IMM','INT','EXT','BUS','INT1']),   'TRIG:SOUR')
         trigger_count           = rlts.SCPI(rlts.Int(min=1,max=200,step=1,help="help me"),      'TRIG:COUN')
         measurement_rate        = rlts.SCPI(rlts.EnumBytes(['NORM','DOUB','FAST']),             'SENS:MRAT')
         sweep_aperture          = rlts.SCPI(rlts.Float(min=20e-6, max=200e-3,label='s'),        'SWE:APER')
         frequency               = rlts.SCPI(rlts.Float(min=10e6, max=18e9,step=1e-3,label='Hz'),'SENS:FREQ')
 
-    def preset (self, block=True):
-        if block:
-            self.link.write('SYST:PRES;*OPC')
-            self.link.query('*OPC?')
-        else:
-            self.link.write('SYST:PRES')
+    def preset (self):
+        self.write('SYST:PRES')
 
     def fetch (self):
         ''' Return a single number or pandas Series containing the power readings
         '''
-        response = self.link.query('FETC?')
-        return pd.Series([float(s) for s in response.split(',')])
+        response = self.query('FETC?').split(',')
+        if len(response)==1:
+            return float(response[0])
+        else:
+            return pd.Series([float(s) for s in response.split(',')])
 
 if __name__ == '__main__':
     from pylab import *
