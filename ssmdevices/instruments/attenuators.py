876037015f6863c648ff6fff2eb0cf79585f61c5
Dan Kuester <dkuester@nist.gov>
2017-03-15 12:54:12 -0600

    fix the attenuator, I think

--- ssmdevices/instruments/attenuators.py
+++ ssmdevices/instruments/attenuators.py
@@ -10,50 +10,51 @@ Created on Tue Mar 07 14:38:10 2017
 from labbench.dotnet import import_dotnet
 import ssmdevices.lib
 
+import logging
+logger = logging.getLogger('labbench')
+
 try:
     dll = import_dotnet('mcl_RUDAT64.dll', ssmdevices.lib)
 except Exception,e:
-    print 'ssmdevices: could not load dll; no support for MiniCircuits variable attenuators'
-    print e
+    print logger.warn('could not load mcl_RUDAT64.dll; MiniCircuits variable attenuator driver will not work')
     
-else:
-    import labbench as core
+import labbench as core
+
+class MiniCircuitsRCDAT(core.Device):
+    ''' A digitally controlled, 0 to 110 dB variable attenuator.
     
-    class MiniCircuitsRCDAT(core.Device):
-        ''' A digitally controlled, 0 to 110 dB variable attenuator.
+        Ensure that the windows DLL driver is installed by copying mcl_RUDAT64.dll from
+        the manufacturer website or install CD into C:\Windows\SysWOW64\.
+ 
+        This implementation calls the .NET drivers provided by the manufacturer
+        instead of the C DLL drivers recommended by the manufacturer in order
+        to support 64-bit python.
+    '''
+    
+    class state(core.Device.state):
+        attenuation = core.Float(min=0, max=115, step=0.25)
         
-            Ensure that the windows DLL driver is installed by copying mcl_RUDAT64.dll from
-            the manufacturer website or install CD into C:\Windows\SysWOW64\.
-     
-            This implementation calls the .NET drivers provided by the manufacturer
-            instead of the C DLL drivers recommended by the manufacturer in order
-            to support 64-bit python.
+        @attenuation.getter
+        def attenuation(self, device):
+            return device.driver.Read_Att(0)[1]
+    
+        @attenuation.setter
+        def attenuation(self, device, value):
+            device.driver.SetAttenuation(value)
+       
+    def connect (self):
+        ''' Open the device resource.
         '''
-        
-        class state(core.Device.state):
-            attenuation = core.Float(min=0, max=115, step=0.25)
-            
-            @attenuation.getter
-            def attenuation(self, device):
-                return device.driver.Read_Att(0)[1]
-        
-            @attenuation.setter
-            def attenuation(self, device, value):
-                device.driver.SetAttenuation(value)
-           
-        def connect (self):
-            ''' Open the device resource.
-            '''
-            self.driver = dll.USB_RUDAT()
-            if self.driver.Connect(self.resource)[0] != 1:
-                raise Exception('Cannot connect to attenuator resource {}'.format(self.resource))
-            self.state.connected = True
-    
-        def disconnect(self):
-            ''' Release the attenuator hardware resource via the driver DLL.
-            '''
-            try:
-                self.driver.Disconnect()
-            except:
-                self.state.connected = False
+        self.driver = dll.USB_RUDAT()
+        if self.driver.Connect(self.resource)[0] != 1:
+            raise Exception('Cannot connect to attenuator resource {}'.format(self.resource))
+        self.state.connected = True
+
+    def disconnect(self):
+        ''' Release the attenuator hardware resource via the driver DLL.
+        '''
+        try:
+            self.driver.Disconnect()
+        except:
+            self.state.connected = False
             self.driver = None
