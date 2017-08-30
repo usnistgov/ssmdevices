891cce033f55b338268ffe9354180161087e1513
Dan Kuester <dkuester@nist.gov>
2017-08-30 13:12:11 -0600

    updates to scripts, and add an example

--- scripts/install prerequisites.bat
+++ scripts/install prerequisites.bat
@@ -1,6 +1,2 @@
-pip uninstall pyserial
-conda install pyvisa pyserial traitlets
-conda install -c conda-forge ipywidgets notebook
-pip install pyvisa-py pythonnet
-conda install -c achennu libusb=1.0.20
+conda install -c conda-forge ipywidgets notebook pandas scipy
 pause
--- scripts/run jupyter notebook server.bat
+++ scripts/run jupyter notebook server.bat
@@ -1,2 +1,3 @@
+call "%userprofile%\AppData\Local\Continuum\Anaconda2\Scripts\activate.bat"
 cd ..
 jupyter notebook
