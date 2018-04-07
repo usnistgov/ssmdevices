196e0fa0b952f631a7d279262811e9e7721d6b28
dkuester <dkuester@nist.gov>
2018-04-07 11:43:40 -0600

    numerous fixes that seem to make the TM500 driver work on tests to cycle things

--- ssmdevices/instruments/networking.py
+++ ssmdevices/instruments/networking.py
@@ -11,6 +11,7 @@ standard_library.install_aliases()
 
 import labbench as lb
 import time, os, ssmdevices.etc
+import numbers
 
 __all__ = ['AeroflexTM500']
 
@@ -41,98 +42,102 @@ class AeroflexTM500(lb.TelnetDevice):
                                     help='ip address of TM500 backend')
         remote_ports = lb.LocalUnicode('5001 5002 5003', is_metadata=True,
                                        help='port of TM500 backend')
+        min_acquisition_time = lb.LocalInt(25, min=0, help='minimum time to spend acquiring logs (s)')
 
         port = lb.LocalInt(5003, min=1, is_metadata=True)
         config_root  = lb.LocalUnicode('', is_metadata=True,
                                        help='path to the command scripts directory')
         data_root  = lb.LocalUnicode(help='remote save root directory',
                                      is_metadata=True)
+        convert_files = lb.LocalList(help='text to match in the filename of data output files to convert')
 
-    def configure(self, name, force=False):
-        ''' Ensure that config script at :param path: has been run.
-            The configuration script loaded is determined as
-            `os.path.join(self.state.config_root, self.state.config_file)+'.conf'`.
+
+    def arm(self, scenario_name):
+        ''' Load the scenario from the command listing in a local TM500
+            configuration file.
+            The the full path to the configuration file is
+            `os.path.join(self.state.config_root, self.state.config_file)+'.conf'`
+            (on the host computer running this python instance).
 
             If the last script that was run is the same as the selected config
             script, then the script is loaded and sent to the TM500 only
             if force=True. It always runs on the first call after AeroflexTM500
             is instantiated.
 
-            :returns: None
+            :returns: A list of responses to each command sent
         '''
-        if name == self.__last.setdefault('config',None) and not force:
-            self.logger.debug('not running script {} because it is already running'\
-                              .format(name))
-            return
-        else:
-            self.__last['config'] = name
-            config_path = os.path.join(self.state.config_root, name)+'.conf'
-            self.logger.debug('loading configuration from {}'\
-                              .format(name))
-        t0 = time.time()
+        if isinstance(scenario_name, numbers.Number):
+            scenario_name = str(scenario_name)
+        if scenario_name == self.__latest.setdefault('scenario_name',None) is not None:
+            raise TM500Error('the TM500 is already armed with the scenario named {}'.format(scenario_name))
 
+        config_path = os.path.join(self.state.config_root, scenario_name)+'.conf'
+        self.logger.debug('arming TM500 scenario {}'\
+                          .format(repr(config_path)))
+
+        t0 = time.time()
         with open(config_path, 'r') as f:
             seq = f.readlines()
+        self._reconnect()
         ret = self._send(seq)
         if self.state.data_root is not None:
             self._send('#$$DATA_LOG_FOLDER 1 "{}"'.format(self.state.data_root))
-        self.logger.debug('run configured in {:.2f}s'.format(time.time()-t0))        
+        self.logger.debug('armed in {:.2f}s'.format(time.time()-t0))
+        self.__latest['scenario_name'] = scenario_name
         return ret
 
-    def start_logging(self):
+    def trigger(self):
         ''' Start logging and return the path to the directory where the data
-            is saved.
+            is being saved.
         '''
-        self.__last['data'] = self._send('#$$START_LOGGING').split("'")[1]
-        return self.__last['data']
+        if self.__latest.get('scenario_name') is None:
+            raise TM500Error('arm before triggering')
+        self._reconnect()
+        self.__latest['data'] = self._send('#$$START_LOGGING').split("'")[1]
+        self.__latest['trigger_time'] = time.time()
+        return self.__latest['data']
 
-    def stop_logging(self, convert=True):
+    def stop(self, convert=True):
         ''' Stop logging.
             :param bool convert: Whether to convert the output binary files to text
             
             :returns: If convert=True, a dictionary of {'name': path} items pointing to the converted text output
         '''
-        if self.__last.setdefault('data', None) is None:
-            self.logger.warning('got request to stop logging, but logging was not started')
-            return {} if convert else None
-        
-        self._send('#$$STOP_LOGGING')
-                   
-        if convert:
-            root = self.__last['data']
-            # Converts the latest dataset to text
-            t0 = time.time()
-#            pre_entries = [f for f in os.listdir(root)]
-            self._send('#$$CONVERT_TO_TEXT')
-            entries = [f for f in os.listdir(root)\
-                       if f.lower().endswith(('csv','txt'))]
-            names = ['_'.join(e.split('_')[3:-1]).replace('-','_')\
-                     for e in entries]
-            paths = [os.path.join(root, e) for e in entries]
-            ret = dict(zip(names,paths))
-            
-            self.logger.info('converted TM500 log from binary in {:0.2f}s'
-                           .format(time.time()-t0))
+        ret = {} if convert else None
+        self._block_until_min_acquisition()
+        try:
+            # Stop running
+            self._reconnect()
+            self._send('forw mte MtsClearMts')
+            self._send('WAIT FOR "I: CMPI MTE 0 MTSCLEARMTS COMPLETE IND" TIMEOUT 60')
+            self._send('forw mte DeConfigRdaStopTestCase')
+            self._send('WAIT FOR "I: CMPI DTE RDA TEST GROUP STOPPED IND" TIMEOUT 300')
+        except TM500Error as e:
+            self.logger.debug('exception on attempt to stop scenario: {}'.format(str(e)))
         else:
-            ret = None
+            if self.__latest.setdefault('data', None) is None:
+                self.logger.debug('there is no active logging to stop')
+            else:
+                self._send('#$$STOP_LOGGING')
+                if convert:
+                    ret = self._convert_to_text()
+                self._send('forw mte GetRrcStats [] [] [] [1] [1]')
+                self._send('forw mte GetNasStats [] [] [] [] [1] [1]')
+        finally:
+            try:
+                self._send('SDLI LTE_RADIO_CONTEXT 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
+                self._send('SDLI LTE_NAS_STATUS 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
+                self._send('SDLI LTE_RRC_STATUS 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
+            except TM500Error as e:
+                self.logger.debug('exception on attempt to cleanup scenario: {}'.format(str(e)))
 
-        self.__last['data'] = None
-        return ret
-
-    def stop_running(self):
-        self._send('forw mte MtsClearMts')
-        self._send('WAIT FOR "I: CMPI MTE 0 MTSCLEARMTS COMPLETE IND" TIMEOUT 60')
-        self._send('forw mte DeConfigRdaStopTestCase')
-        self._send('WAIT FOR "I: CMPI DTE RDA TEST GROUP STOPPED IND" TIMEOUT 300')
-        self._send('#$$STOP_LOGGING')
-        self._send('forw mte GetRrcStats [] [] [] [1] [1]')
-        self._send('forw mte GetNasStats [] [] [] [] [1] [1]')
-        self._send('SDLI LTE_RADIO_CONTEXT 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
-        self._send('SDLI LTE_NAS_STATUS 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
-        self._send('SDLI LTE_RRC_STATUS 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000 0x00000000')
-
+        self.__latest['scenario_name'] = None
+        self.__latest['data'] = None
+        self.__latest['trigger_time'] = 0
+        return ret        
+    
     @staticmethod
-    def screensave_to_script(path):
+    def command_log_to_script(path):
         ''' Scrape a script out of a TM500 "screen save" text file. The output
             for an input that takes the form <path>/<to>/<filename>.txt
             will be <path>/<to>/<filename>-script.txt.
@@ -142,18 +147,24 @@ class AeroflexTM500(lb.TelnetDevice):
         with open(path, 'rb') as f:
             txt = f.read()
             
-        # Assume tab-delimited lines include a timestamp. Remove the timestamp.
+        # Remove the timestamp.
+        # Assumes tab-delimiter separtaes timestamps from the telnet traffic. 
         txt = b'\r\n'.join([line.split(b'\t',1)[-1] for line in txt.splitlines()])
         
         # start where things get interesting
-        i = txt.lower().find(b'abot 0 0 0')
-        if i != -1:
-            txt = txt[i:]
+#        i = txt.lower().find(b'abot 0 0 0')
+        lines = txt.splitlines()
+        for i,line in enumerate(lines):
+            if line.lower().strip().startswith(b'rset'):
+                break
+        else:
+            raise ValueError('no start delimited by RSET command found in command log')
+        txt = b'\r\n'.join(lines[i:])
 
         # end at activate
         i = txt.upper().find(b'C: FORW 0X00 OK MTE ACTIVATE')
         if i == -1:
-            raise ValueError('no activate found in script')
+            raise ValueError('no activate found in command log')
         i_eol = txt[i:].find(b'\r\n')
         if i_eol != -1:
             i = i+i_eol
@@ -195,41 +206,67 @@ class AeroflexTM500(lb.TelnetDevice):
             f.write(b'\r\n'.join(commands))
 
     def disconnect(self):
-        try:
-            self.stop_running()
-        except TM500Error as e:
-            if e.errcode not in (0x02, 0x06):
-                raise
+        if self.__latest['scenario_name'] is not None:
+            try:
+                self.stop(convert=False)
+            except TM500Error as e:
+                if e.errcode not in (0x02, 0x06):
+                    raise
 
         try:
             self._send('#$$DISCONNECT', confirm=False)
         except:
             pass
-        
+
         super(AeroflexTM500, self).disconnect()
 
     def setup(self):
         # Invalidate any incomplete previous commands in the remote telnet buffer
-#        try:
-#            self._send('***', timeout=1)
-#        except (ValueError,TimeoutError):
-#            pass
-
-        # Ensure we are disconnected
         try:
-            self._send('#$$DISCONNECT', timeout=1)
+            self._send('***', timeout=1)
         except (ValueError,TimeoutError):
             pass
+        self.__latest = {}
+        self._reconnect()
+        
+        # Set up licensing
+        self._send('ABOT 0 0 0')
+        self._send('SCFG SWL')
+        self._send('STRT')
+        self._send('forw swl setlicenseserver none')
 
-        # Now connect
-        self._send('#$$PORT {ip} {ports}'\
-                  .format(ip=self.state.remote_ip, ports=self.state.remote_ports),
-                  timeout=1)
-        self._send('#$$CONNECT')
-        self.__last = {}
-#        self.__last_config = None
-#        self.__last_data = None
+    def _tma_is_connected(self):
+        try:
+            self._send('CHOW')
+        except TM500Error as e:
+            if e.errcode in (0x02, 0x06):
+                return False
+            else:
+                raise
+        return True
 
+    def _reconnect(self):
+        ''' Ensure the TMA software is connected to the TM500
+        '''
+        if self._tma_is_connected():
+            return
+        else:
+            # Now connect
+            self._send('#$$PORT {ip} {ports}'\
+                      .format(ip=self.state.remote_ip, ports=self.state.remote_ports),
+                      timeout=1)
+            self._send('#$$CONNECT')
+            
+    def _block_until_min_acquisition(self):
+        ''' Make sure the minimum acquisition time has elapsed to avoid putting
+            the TM500 in a sad state
+        '''
+
+        if self.__latest.get('trigger_time',0) > 0:
+            elapsed = time.time()-self.__latest.get('trigger_time',0)
+            if elapsed < self.state.min_acquisition_time:
+                time.sleep(self.state.min_acquisition_time-elapsed)
+        
     def _send(self, msg, data_lines=1, confirm=True, timeout=None):
         ''' Send a message, then block until a confirmation message is received.
         
@@ -284,6 +321,36 @@ class AeroflexTM500(lb.TelnetDevice):
                 self.logger.debug('extra -> ({} lines)'.format(extra.count(b'\r')))
         return ret.decode('ascii')
 
+    def _convert_to_text(self):
+        ''' Convert the latest data to text
+        '''
+        root = self.__latest['data']
+
+        t0 = time.time()
+        time.sleep(0.25) # TODO Is this really necessary?
+#            pre_entries = [f for f in os.listdir(root)]
+        self._send('#$$CONVERT_TO_TEXT')
+        entries = [f for f in os.listdir(root)\
+                   if f.lower().endswith(('csv','txt'))]
+        names = ['_'.join(e.split('_')[3:-1]).replace('-','_')\
+                 for e in entries]
+        paths = [os.path.join(root, e) for e in entries]
+        ret = dict(zip(names,paths))
+        
+        # Remove items not listed in self.state.convert_files
+        if len(self.state.convert_files)>0:
+            for name in ret.keys():
+                for inc in self.state.convert_files:
+                    if inc.upper() in name.upper():
+                        break
+                else:
+                    del ret[name]
+        
+        self.logger.info('converted TM500 logs from binary in {:0.2f}s'
+                       .format(time.time()-t0))
+
+        return ret
+
     def _read_until (self, rsp, timeout=None):
         if timeout is None:
             ack_timeout = self.state.ack_timeout
@@ -307,7 +374,7 @@ class AeroflexTM500(lb.TelnetDevice):
         return ret
 
 if __name__ == '__main__':
-    AeroflexTM500.screensave_to_script(r'C:\Users\dkuester\Desktop\TM500_2Sec_8UEs_withTime.txt')
+    AeroflexTM500.command_log_to_script(r'C:\Users\dkuester\Desktop\TM500_2Sec_8UEs_withTime.txt')
     
 #    path = r'e:\TM500ScriptForPaulDebug'
 #    lb.show_messages('debug')
