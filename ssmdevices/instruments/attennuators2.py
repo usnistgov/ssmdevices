8201e1023dc6d8e203b704adb35e41369dc114c0
ynm5 <yao.ma@nist.gov>
2018-04-20 18:50:09 -0400

    Update attennuators2.py

--- ssmdevices/instruments/attennuators2.py
+++ ssmdevices/instruments/attennuators2.py
@@ -9,7 +9,7 @@ Created on Fri Apr 20 16:06:39 2018
 """
 Created on Tue Mar 07 14:38:10 2017
 
-@author: dkuester
+ 
 """
 from __future__ import unicode_literals
 from __future__ import print_function
@@ -18,7 +18,7 @@ from __future__ import absolute_import
 
 from future import standard_library
 standard_library.install_aliases()
-__all__ = ['MiniCircuitsRCDAT']
+__all__ = ['MiniCircuitsRCDAT2']
 
 from labbench import DotNetDevice
 import ssmdevices.lib
