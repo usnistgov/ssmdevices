7cf70292378975b38e6e28832b9c442ccc4737c0
Dan Kuester <dkuester@nist.gov>
2018-08-24 13:55:50 -0600

    script and example notebook fixes

--- scripts/run jupyter notebook server.bat
+++ scripts/run jupyter notebook server.bat
@@ -1,4 +1,6 @@
 call "%userprofile%\AppData\Local\Continuum\Anaconda2\Scripts\activate.bat"
 call "%userprofile%\AppData\Local\Continuum\Anaconda3\Scripts\activate.bat"
+call C:\ProgramData\Anaconda2\Scripts\activate.bat"
+call C:\ProgramData\Anaconda3\Scripts\activate.bat"
 cd ..
 jupyter notebook
