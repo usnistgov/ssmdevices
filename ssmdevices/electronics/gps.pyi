1912daa5221e59c09c0213b3a45db86e9e80aa85
Dan Kuester <daniel.kuester@nist.gov>
2021-11-15 13:57:23 -0700

    stub update

--- ssmdevices/electronics/gps.pyi
+++ ssmdevices/electronics/gps.pyi
@@ -1,21 +1,24 @@
 import labbench as lb
 from typing import Any
 
+
 class SwiftNavPiksi(lb.SerialLoggingDevice):
+
     def __init__(
         self,
-        resource: str = "str",
-        timeout: str = "int",
-        write_termination: str = "bytes",
-        baud_rate: str = "int",
-        parity: str = "bytes",
-        stopbits: str = "int",
-        xonxoff: str = "bool",
-        rtscts: str = "bool",
-        dsrdtr: str = "bool",
-        poll_rate: str = "float",
-        data_format: str = "bytes",
-        stop_timeout: str = "float",
-        max_queue_size: str = "int",
-    ): ...
+        resource: str='str',
+        timeout: str='int',
+        write_termination: str='bytes',
+        baud_rate: str='int',
+        parity: str='bytes',
+        stopbits: str='int',
+        xonxoff: str='bool',
+        rtscts: str='bool',
+        dsrdtr: str='bool',
+        poll_rate: str='float',
+        data_format: str='bytes',
+        stop_timeout: str='float',
+        max_queue_size: str='int'
+    ):
+        ...
     baud_rate: Any = ...
