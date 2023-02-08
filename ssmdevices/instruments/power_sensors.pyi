e077f33c5c34c6eb32fce6fea9a8e2ee2a2ac209
Kuester <dkuester@nist.gov>
2023-02-08 08:01:34 -0700

    update stubs

--- ssmdevices/instruments/power_sensors.pyi
+++ ssmdevices/instruments/power_sensors.pyi
@@ -1,16 +1,13 @@
 import labbench as lb
 from _typeshed import Incomplete
 
-
 class KeysightU2000XSeries(lb.VISADevice):
-
     def __init__(
         self,
-        resource: str='str',
-        read_termination: str='str',
-        write_termination: str='str'
-    ):
-        ...
+        resource: str = "str",
+        read_termination: str = "str",
+        write_termination: str = "str",
+    ): ...
     TRIGGER_SOURCES: Incomplete
     initiate_continuous: Incomplete
     output_trigger: Incomplete
@@ -20,31 +17,20 @@ class KeysightU2000XSeries(lb.VISADevice):
     sweep_aperture: Incomplete
     frequency: Incomplete
 
-    def preset(self) -> None:
-        ...
-
-    def fetch(self):
-        ...
-
+    def preset(self) -> None: ...
+    def fetch(self): ...
     @classmethod
-    def __imports__(cls) -> None:
-        ...
-
+    def __imports__(cls) -> None: ...
 
 class RohdeSchwarzNRPSeries(lb.VISADevice):
-
-    def __init__(self, resource: str='str', write_termination: str='str'):
-        ...
+    def __init__(self, resource: str = "str", write_termination: str = "str"): ...
     FUNCTIONS: Incomplete
     TRIGGER_SOURCES: Incomplete
     frequency: Incomplete
     initiate_continuous: Incomplete
 
-    def function(self, value) -> None:
-        ...
-
-    def trigger_source(self):
-        ...
+    def function(self, value) -> None: ...
+    def trigger_source(self): ...
     trigger_delay: Incomplete
     trigger_count: Incomplete
     trigger_holdoff: Incomplete
@@ -62,18 +48,10 @@ class RohdeSchwarzNRPSeries(lb.VISADevice):
     smoothing_enable: Incomplete
     read_termination: Incomplete
 
-    def preset(self) -> None:
-        ...
-
-    def trigger_single(self) -> None:
-        ...
-
-    def fetch(self):
-        ...
-
-    def fetch_buffer(self):
-        ...
-
+    def preset(self) -> None: ...
+    def trigger_single(self) -> None: ...
+    def fetch(self): ...
+    def fetch_buffer(self): ...
     def setup_trace(
         self,
         frequency,
@@ -81,24 +59,15 @@ class RohdeSchwarzNRPSeries(lb.VISADevice):
         sample_period,
         trigger_level,
         trigger_delay,
-        trigger_source
-    ) -> None:
-        ...
-
+        trigger_source,
+    ) -> None: ...
     @classmethod
-    def __imports__(cls) -> None:
-        ...
-
+    def __imports__(cls) -> None: ...
 
 class RohdeSchwarzNRP8s(RohdeSchwarzNRPSeries):
-
-    def __init__(self, resource: str='str', write_termination: str='str'):
-        ...
+    def __init__(self, resource: str = "str", write_termination: str = "str"): ...
     frequency: Incomplete
 
-
 class RohdeSchwarzNRP18s(RohdeSchwarzNRPSeries):
-
-    def __init__(self, resource: str='str', write_termination: str='str'):
-        ...
+    def __init__(self, resource: str = "str", write_termination: str = "str"): ...
     frequency: Incomplete
