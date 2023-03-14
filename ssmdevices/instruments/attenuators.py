2c7555eea025ed7b54b4e315445435c37f4c54dd
mkforsyth-nist <keith.forsyth@nist.gov>
2023-03-14 14:05:51 -0600

    Update attenuators.py
    Addition of if statements to merge together four channel and single channel attenuators.

--- ssmdevices/instruments/attenuators.py
+++ ssmdevices/instruments/attenuators.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["MiniCircuitsRCDAT", "MiniCircuitsRC4DAT"]
+__all__ = ["MiniCircuitsRCDAT"]
 
+import ssmdevices.lib
 import labbench as lb
+import time, random
 
 if __name__ == "__main__":
     from _minicircuits_usb import SwitchAttenuatorBase
@@ -38,6 +40,15 @@ class MiniCircuitsRCDAT(SwitchAttenuatorBase):
         "(row) and attenuation setting (column)), or None to search ssmdevices",
     )
 
+    channel = lb.value.int(
+        default=None,
+        allow_none=True,
+        min=1,
+        max=4,
+        cache=True,
+        help="a port selector for 4 port attenuators None is a single attenuator",
+    )
+
     # the only property that directly sets attenuation in the device
     @lb.property.float(
         min=0, max=115, step=0.25, label="dB", help="uncalibrated attenuation"
@@ -46,19 +57,36 @@ class MiniCircuitsRCDAT(SwitchAttenuatorBase):
         # getter
         CMD_GET_ATTENUATION = 18
 
-        d = self._cmd(CMD_GET_ATTENUATION)
-        full_part = d[1]
-        frac_part = float(d[2]) / 4.0
-        return full_part + frac_part
+        if self.channel is None:
+            d = self._cmd(CMD_GET_ATTENUATION)
+            full_part = d[1]
+            frac_part = float(d[2]) / 4.0
+            return full_part + frac_part
+        elif self.channel in range(1,5):
+            d = self._cmd(CMD_GET_ATTENUATION)
+            offs = self.channel * 2 - 1
+            full_part = d[offs]
+            frac_part = float(d[offs + 1]) / 4.0
+            return full_part + frac_part
+        else:
+            raise AttributeError
 
     @attenuation_setting
     def attenuation_setting(self, value):
         # setter
         CMD_SET_ATTENUATION = 19
 
-        value1 = int(value)
-        value2 = int((value - value1) * 4.0)
-        self._cmd(CMD_SET_ATTENUATION, value1, value2, 1)
+        if self.channel is None:
+            value1 = int(value)
+            value2 = int((value - value1) * 4.0)
+            self._cmd(CMD_SET_ATTENUATION, value1, value2, 1)
+        elif self.channel in range(1,5):
+            value1 = int(value)
+            value2 = int((value - value1) * 4.0)
+            self._cmd(CMD_SET_ATTENUATION, value1, value2, self.channel)
+            time.sleep(0.001) #this prevents collisions for the moment
+        else:
+            raise AttributeError
 
     # the remaining traits are calibration corrections for attenuation_setting
     attenuation = attenuation_setting.calibrate_from_table(
@@ -75,39 +103,34 @@ class MiniCircuitsRCDAT(SwitchAttenuatorBase):
     )
 
 
-class MiniCircuitsRC4DAT(SwitchAttenuatorBase):
-    _PID = 0x23
-
-    CMD_GET_ATTENUATION = 18
-    CMD_SET_ATTENUATION = 19
-
-    attenuation1 = lb.property.float(min=0, max=115, step=0.25, key=1)
-    attenuation2 = lb.property.float(min=0, max=115, step=0.25, key=2)
-    attenuation3 = lb.property.float(min=0, max=115, step=0.25, key=3)
-    attenuation4 = lb.property.float(min=0, max=115, step=0.25, key=4)
-
-    def get_key(self, key, trait_name=None):
-        d = self._cmd(self.CMD_GET_ATTENUATION)
-        offs = key * 2 - 1
-        full_part = d[offs]
-        frac_part = float(d[offs + 1]) / 4.0
-        return full_part + frac_part
-
-    def set_key(self, key, value, trait_name=None):
-        value1 = int(value)
-        value2 = int((value - value1) * 4.0)
-        self._cmd(self.CMD_SET_ATTENUATION, value1, value2, key)
-
-
 if __name__ == "__main__":
     import numpy as np
+    check_single_channel = False
+    check_four_channel = True
+    resource = '12208250156'
 
     lb.show_messages("info")
 
-    for i in np.arange(0, 110.25, 5):
-        atten = MiniCircuitsRCDAT("11604210014", frequency=5.3e9)
-        atten2 = MiniCircuitsRCDAT("11604210008", frequency=5.3e9)
-        with atten, atten2:
-            atten.attenuation = i
-            # print(atten.output_power)
-            lb.logger.info(str(atten.attenuation))
+    if check_single_channel:
+        for i in np.arange(0, 110.25, 5):
+            atten = MiniCircuitsRCDAT("11604210014", frequency=5.3e9)
+            with atten:
+                atten.attenuation = i
+                # print(atten.output_power)
+                lb.logger.info(str(atten.attenuation))
+
+    if check_four_channel:
+        for i in np.arange(0, 110.25, 5):
+            atten1 = MiniCircuitsRCDAT(resource, frequency=5.3e9, channel=1)
+            atten2 = MiniCircuitsRCDAT(resource, frequency=5.3e9, channel=2)
+            atten3 = MiniCircuitsRCDAT(resource, frequency=5.3e9, channel=3)
+            atten4 = MiniCircuitsRCDAT(resource, frequency=5.3e9, channel=4)
+            with atten1,atten2,atten3,atten4:
+                atten1.attenuation_setting = i
+                atten2.attenuation_setting = i
+                atten3.attenuation_setting = i
+                atten4.attenuation_setting = i
+                print(f"Attenuator 1 set point {str(atten1.attenuation_setting)}")
+                print(f"Attenuator 2 set point {str(atten2.attenuation_setting)}")
+                print(f"Attenuator 3 set point {str(atten3.attenuation_setting)}")
+                print(f"Attenuator 4 set point {str(atten4.attenuation_setting)}")
