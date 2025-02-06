1c50cc154a31ce9e322de4fe1c0986013c51d960
jlb20 <jordan.bernhardt@nist.gov>
2025-02-06 14:45:16 -0700

    stash changes

--- src/ssmdevices/instruments/base_station_emulators.py
+++ src/ssmdevices/instruments/base_station_emulators.py
@@ -15,6 +15,10 @@ import re
 __all__ = ['RohdeSchwarzCMW500']
 
 default_cmw_address = 'TCPIP0::10.0.0.9::inst0::INSTR'
+def field_as_int(x):
+    if x == "ZERO":
+        return 0
+    return int(re.sub('[^0-9]','', x))
 
 
 @attr.visa_keying(remap={False: 'OFF', True: 'ON'})
@@ -33,7 +37,7 @@ class RohdeSchwarzCMW500(lb.VISADevice):
     @attr.property.int(min=1, max=255)
     def operating_band(self):
         resp = self.query('CONF:LTE:SIGN1:PCC:BAND?')
-        return int(re.sub('[^0-9]','', resp))
+        return field_as_int(resp)
 
     @operating_band.setter
     def _(self, int_value):
@@ -55,7 +59,7 @@ class RohdeSchwarzCMW500(lb.VISADevice):
     @attr.property.int(only=(1.4, 3, 5, 10, 15, 20))
     def cell_bw_mhz(self):
         resp = self.query('CONFigure:LTE:SIGN:CELL:BANDwidth:PCC:DL?')
-        return int(re.sub('[^0-9]','', resp))
+        return field_as_int(resp)
 
     @cell_bw_mhz.setter
     def _(self, int_val):
@@ -78,10 +82,118 @@ class RohdeSchwarzCMW500(lb.VISADevice):
     scheduling_type = attr.property.str(
         key='CONF:LTE:SIGN1:CONN:PCC:STYPE', only=['RMC', 'UDCH', 'UDTT', 'CQI', 'EMAM', 'EMCS']
     )
-
     # Downlink allocation parameters, rb and modulation configuration
-    dl_num_rbs = attr.property.int(key='CONFigure:LTE:SIGN:CONNection:PCC:RMC:DL', min=0, max=100)
+    def _get_dl_params(self):
+        """
+        Private function retrieve downlink parameters independent of scheduling type
+        Be carful, querying each type of scheduling type will change the set scheduling type, this logic takes care of that
 
+        returns: dictionary with all of the fields for getting/setting the downlink parameters
+        """
+        # TODO: hmm we could refactor this func to input a dl_params dict to either query or write commands...
+        sched_type = self.scheduling_type
+        field_dict = {"scheduling": sched_type}
+        try:
+            # Python requirment for ssmdevices is 3.9-3.12 TODO: update to python >= 3.10 for match, case
+            if sched_type == "RMC":
+                vals = self.query('CONFigure:LTE:SIGN:CONNection:PCC:RMC:DL?')
+                fields = vals.split(',')
+                if fields[2] == "ZERO":
+                    fields[2] = "0"
+                field_dict.update({'num_rbs': field_as_int(fields[0]), 'modulation': fields[1], 'transblkszidx': fields[2]})
+            elif sched_type == "UDCH":
+                # TODO: implement more than one DL channel, currently set to DL1
+                vals = self.query('CONFigure:LTE:SIGN:CONNection:PCC:UDCHannels:DL1?')
+                fields = vals.split(',')
+                # for some god-awful reason, num_rbs reported here is numeric rather than format 'N[x]' (where x is numeric)
+                field_dict.update({'num_rbs': fields[0],'start_rb': fields[1], 'modulation': fields[2], 'transblkszidx': fields[3]})
+            elif sched_type == "UDTT":
+                raise NotImplementedError("UDTT scheduling type not handled")
+            elif sched_type == "CQI":
+                raise NotImplementedError("CQI scheduling type not handled")
+            elif sched_type == "EMAM":
+                raise NotImplementedError("EMAM scheduling type not handled")
+            elif sched_type == "EMCS":
+                raise NotImplementedError("EMCS scheduling type not handled")
+            else:
+                raise ValueError(f"Unexpected scheduling type returned: {sched_type}")
+        except Exception as e:
+            try:
+                print(fields)
+            except NameError:
+                raise e("Error somewhere parsing")
+            finally:
+                raise Exception
+
+        return field_dict
+
+    def _format_dl_params(self, dl_param_dict):
+        sched_type = self.scheduling_type
+        if sched_type == "RMC":
+            output_string = "N{},{},T{}".format(dl_param_dict['num_rbs'], dl_param_dict['modulation'], dl_param_dict['transblkszidx'])
+        elif sched_type == "UDCH":
+            output_string = "{},{},{},{}".format(dl_param_dict['num_rbs'], dl_param_dict['start_rb'], dl_param_dict['modulation'], dl_param_dict['transblkszidx'])
+        elif sched_type in ("UDTT", "CQI", "EMAM", "EMCS"):
+            raise NotImplementedError(f"{sched_type} scheduling type not handled")
+        else:
+            raise ValueError(f"Unexpected scheduling type returned: {sched_type}")
+
+        return output_string
+
+    @attr.property.int(min=0, max=100)
+    def dl_num_rbs(self):
+        return self._get_dl_params()['num_rbs']
+
+    @dl_num_rbs.setter
+    def _(self, int_val):
+        dl_params = self._get_dl_params()
+        if dl_params['scheduling'] == 'RMC':
+            dl_params['num_rbs'] = int_val
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:RMC:DL {self._format_dl_params(dl_params)}')
+        elif dl_params['scheduling'] == 'UDCH':
+            dl_params['num_rbs'] = int_val
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:UDCHannels:DL1 {self._format_dl_params(dl_params)}')
+        elif dl_params['scheduling'] in ("UDTT", "CQI", "EMAM", "EMCS"):
+            raise NotImplementedError(f"{dl_params['scheduling']} scheduling type not handled")
+        else:
+            raise ValueError(f"Unexpected scheduling type returned: {dl_params['scheduling']}")
+
+    @attr.property.any()
+    def dl_start_rb(self):
+        dl_params = self._get_dl_params()
+        if dl_params['scheduling'] == 'RMC':
+            return self.query('CONFigure:LTE:SIGN:CONNection:PCC:RMC:RBPosition:DL?')
+        elif dl_params['scheduling'] == 'UDCH':
+            return dl_params['start_rb']
+        else:
+            raise NotImplementedError(f"{dl_params['scheduling']} scheduling type not handled")
+
+    @dl_start_rb.setter
+    def _(self, val):
+        dl_params = self._get_dl_params()
+        if dl_params['scheduling'] == 'RMC':
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:RMC:RBPosition:DL {val}')
+        elif dl_params['scheduling'] == 'UDCH':
+            dl_params['start_rb'] = val
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:UDCHannels:DL1 {self._format_dl_params(dl_params)}')
+        else:
+            raise NotImplementedError(f"{dl_params['scheduling']} scheduling type not handled")
+
+    @attr.property.str(only=('QPSK', 'Q16', 'Q64', 'Q256'))
+    def dl_modulation(self):
+        dl_params = self._get_dl_params()
+        return dl_params['modulation']
+
+    @dl_modulation.setter
+    def _(self, str_val):
+        dl_params = self._get_dl_params()
+        setter_string = self._format_dl_params(dl_params)
+        if dl_params['scheduling'] == 'RMC':
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:RMC:DL {setter_string}')
+        elif dl_params['scheduling'] == 'UDCH':
+            self.write(f'CONFigure:LTE:SIGN:CONNection:PCC:UDCHannels:DL1 {setter_string}')
+        else:
+            raise NotImplementedError(f"{dl_params['scheduling']} scheduling type not handled")
 
     # Uplink allocation parameters, rb and modulation configuration
     ulrmc_num_rbs = attr.property.int(key='CONF:LTE:SIGN1:CONN:PCC:RMCUL', min=1, max=100)
@@ -304,8 +416,15 @@ def test_cmw():
     # print(cmw500.operating_band)
     cmw500.duplex_mode = "FDD"
     cmw500.cell_bw_mhz = 20
-    cmw500.ulrmc_transblocksize = 16
+    # cmw500.ulrmc_transblocksize = 16
+    print(cmw500.dl_num_rbs)
+    cmw500.dl_num_rbs=100
+    print(cmw500.dl_num_rbs)
+    print(cmw500.dl_start_rb)
     cmw500.dl_num_rbs = 16
+    cmw500.scheduling_type = "RMC"
+    cmw500.dl_start_rb = 0
+    cmw500.dl_modulation = "Q256"
 
     cmw500.close()
     return True
